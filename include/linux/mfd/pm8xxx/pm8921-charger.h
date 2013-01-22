385addda55a7c665a39eec2f56a3e3215982ffb0
Xiaozhe Shi <xiaozhes@codeaurora.org>
2013-01-22 09:46:45 -0800

    msm: power: Revert LGE power patches
    Revert the following customer changes in order to clean up this
    branch.
    
    This reverts commit 33ddf5a9eaa031c00b00846458f2ba534e6807b8.
    This reverts commit f17df2402b795faf2c03de6d8a5b12378674f3be.
    This reverts commit 0a488464c03806e1f775c35d7b2c83650eaab4ef.
    This reverts commit 23d2a7cd02b081e1cf2578c02b7ecd3a65319d4e.
    This reverts commit 90e32fa7cb701d922825e40b82b566e4af843ea2.
    This reverts commit 7346d0fbd3c338d55330766e0f1867f19b364fed.
    This reverts commit eb59bf66db9f2ca6e79552f4f72aa56f8c6abd27.
    This reverts commit 9c56de000bb2390191fa2ddaea738e6d3e8f5d4f.
    This reverts commit 7a72ca62c3f64c7b914edd8d9dbb75933b3c845e.
    This reverts commit 76048dac5085eca4459ee5ea5dd4876a1f518cfa.
    This reverts commit bfafe7ec7cdeef5b44b32f9aa6ee03beba2020e0.
    This reverts commit e346f7a7b8ead1bbb106f4eb1e766c49407bffde.
    This reverts commit 1265899c012a463cb0521ed73f2abe057692a9db
    This reverts commit 1ff608a341b87defa586ca4a24d0bd89abfdfbb3
    This reverts commit 5f030effc8c1af159ad13e77d4c169f037d6c9e4.
    This reverts commit 19c87ddde66fd6aad61a60e466a75af235fca7f9.
    This reverts commit 905eac978c30c96c5d12a81c1f6968d8c57a645f.
    This reverts commit 1bd7c8643cdd8fdc7cf649ec465cc454315a6753.
    This reverts commit 69f1730e0d249bad3b4f0fc0f94d917c16c3df19.
    This reverts commit 2073afb7ee8b3f80f1f3184bd7ac99ead581adee
    This reverts commit 8126554cf049d13f4dd2d815d86ea289efdc6f3a.
    This reverts commit 45ccbb53cfc227fd3811e71b0bd813432c96ee9e
    This reverts commit 325995993eb806285a35381e803ed5432fe5a93a.
    This reverts commit 84cf5a3959bcf412c4b48f8a29b8255c18602d1b.
    This reverts commit 839d5a50c761787ded6b8558fa37696be81196fe.
    This reverts commit d4b1fb57463d04aeca5ae9bfd2181faa016f97c2.
    This reverts commit 2ab04b0deb912a5a52899a50ced73e36974a6a00.
    This reverts commit 3a3de0ee621625c8767e0d3c7106c52fcf834499.
    This reverts commit f278add027723a90b11c9a97f6d1b09c1aaec795.
    This reverts commit 1a749b700752efcc8d832f7b83554df2771dce53.
    
    Change-Id: I21863586360b6bc57dd6d631c34725bea963cffa
    Signed-off-by: Xiaozhe Shi <xiaozhes@codeaurora.org>

--- include/linux/mfd/pm8xxx/pm8921-charger.h
+++ include/linux/mfd/pm8xxx/pm8921-charger.h
@@ -63,7 +63,6 @@ enum pm8921_chg_led_src_config {
  * @ttrkl_time:		max trckl charging time in minutes
  *			valid range 1 to 64 mins. PON default 15 min
  * @update_time:	how often the userland be updated of the charging (msec)
- * @alarm_voltage:	the voltage (mV) when lower battery alarm is triggered
  * @max_voltage:	the max voltage (mV) the battery should be charged up to
  * @min_voltage:	the voltage (mV) where charging method switches from
  *			trickle to fast. This is also the minimum voltage the
@@ -129,7 +128,6 @@ struct pm8921_charger_platform_data {
 	unsigned int			max_voltage;
 	unsigned int			min_voltage;
 	unsigned int			uvd_thresh_voltage;
-	unsigned int			alarm_voltage;
 	unsigned int			resume_voltage_delta;
 	unsigned int			term_current;
 	int				cool_temp;
@@ -138,7 +136,6 @@ struct pm8921_charger_platform_data {
 	unsigned int			max_bat_chg_current;
 	unsigned int			cool_bat_chg_current;
 	unsigned int			warm_bat_chg_current;
-	int				ext_batt_temp_monitor;
 	unsigned int			cool_bat_voltage;
 	unsigned int			warm_bat_voltage;
 	unsigned int			(*get_batt_capacity_percent) (void);
@@ -158,7 +155,6 @@ struct pm8921_charger_platform_data {
 	enum pm8921_chg_hot_thr		hot_thr;
 	int				rconn_mohm;
 	enum pm8921_chg_led_src_config	led_src_config;
-	int				eoc_check_soc;
 };
 
 enum pm8921_charger_source {
@@ -287,11 +283,6 @@ int pm8921_usb_ovp_set_hystersis(enum pm8921_usb_debounce_time ms);
  *
  */
 int pm8921_usb_ovp_disable(int disable);
-
-int pm8921_get_batt_state(void);
-int pm8921_force_start_charging(void);
-int pm8921_get_batt_health(void);
-
 /**
  * pm8921_is_batfet_closed - battery fet status
  *
@@ -299,12 +290,6 @@ int pm8921_get_batt_health(void);
  * batfet this will return 0.
  */
 int pm8921_is_batfet_closed(void);
-#ifdef CONFIG_WIRELESS_CHARGER
-int set_wireless_power_supply_control(int value);
-#endif
-
-int pm8921_set_ext_battery_health(int health, int i_limit);
-
 #else
 static inline void pm8921_charger_vbus_draw(unsigned int mA)
 {
