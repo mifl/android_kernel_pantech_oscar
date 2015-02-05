MiCode/Xiaomi_Kernel_OpenSource

msm: pil: Allow proxy unvoting in error scenario
There are two scenarios where the proxy vote for
a peripheral won't be removed if it fails to boot:

1) When the module parameter does not override
the timeout as specified in the pil descriptor.

2)  When the module parameter is overridden to a
zero value.

So, fix these bugs by changing the pil_proxy_unvote()
function so that it removes the proxy vote when
either of the two above scenarios occur.

Change-Id: Ib796afccca462ff574f6058e93bab747df230ea8
Signed-off-by: Seemanta Dutta <seemanta@codeaurora.org>
Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

arch/arm/mach-msm/peripheral-loader.c
@@ -208,16 +208,24 @@ static int pil_proxy_vote(struct pil_desc *desc)
 	return ret;
 }
 
-static void pil_proxy_unvote(struct pil_desc *desc, unsigned long timeout)
+static void pil_proxy_unvote(struct pil_desc *desc, int immediate)
 {
 	struct pil_priv *priv = desc->priv;
+	unsigned long timeout;
 
-	if (proxy_timeout_ms >= 0)
+	if (proxy_timeout_ms == 0 && !immediate)
+		return;
+	else if (proxy_timeout_ms > 0)
 		timeout = proxy_timeout_ms;
+	else
+		timeout = desc->proxy_timeout;
 
-	if (timeout && desc->ops->proxy_unvote) {
+	if (desc->ops->proxy_unvote) {
 		if (WARN_ON(!try_module_get(desc->owner)))
 			return;
+
+		if (immediate)
+			timeout = 0;
 		schedule_delayed_work(&priv->proxy, msecs_to_jiffies(timeout));
 	}
 }
@@ -558,7 +566,6 @@ int pil_boot(struct pil_desc *desc)
 	const struct elf32_hdr *ehdr;
 	struct pil_seg *seg;
 	const struct firmware *fw;
-	unsigned long proxy_timeout = desc->proxy_timeout;
 	struct pil_priv *priv = desc->priv;
 
 	/* Reinitialize for new image */
@@ -633,12 +640,11 @@ int pil_boot(struct pil_desc *desc)
 	ret = desc->ops->auth_and_reset(desc);
 	if (ret) {
 		pil_err(desc, "Failed to bring out of reset\n");
-		proxy_timeout = 0; /* Remove proxy vote immediately on error */
 		goto err_boot;
 	}
 	pil_info(desc, "Brought out of reset\n");
 err_boot:
-	pil_proxy_unvote(desc, proxy_timeout);
+	pil_proxy_unvote(desc, ret);
 release_fw:
 	release_firmware(fw);
 out:

mifl/android_kernel_pantech_oscar

arch/arm/mach-msm/peripheral-loader.c
@@ -134,13 +134,22 @@ static int pil_proxy_vote(struct pil_device *pil)
 	return ret;
 }
 
-static void pil_proxy_unvote(struct pil_device *pil, unsigned long timeout)
+static void pil_proxy_unvote(struct pil_device *pil, int immediate)
 {
-	if (proxy_timeout_ms >= 0)
-		timeout = proxy_timeout_ms;
+	unsigned long timeout;
 
-	if (timeout && pil->desc->ops->proxy_unvote)
+	if (proxy_timeout_ms == 0 && !immediate)
+		return;
+	else if (proxy_timeout_ms > 0)
+		timeout = proxy_timeout_ms;
+	else
+		timeout = pil->desc->proxy_timeout;
+
+	if (pil->desc->ops->proxy_unvote) {
+		if (immediate)
+			timeout = 0;
 		schedule_delayed_work(&pil->proxy, msecs_to_jiffies(timeout));
+	}
 }
 
 #define IOMAP_SIZE SZ_4M
@@ -255,7 +264,6 @@ static int load_image(struct pil_device *pil)
 	struct elf32_hdr *ehdr;
 	const struct elf32_phdr *phdr;
 	const struct firmware *fw;
-	unsigned long proxy_timeout = pil->desc->proxy_timeout;
 
 	down_read(&pil_pm_rwsem);
 	snprintf(fw_name, sizeof(fw_name), "%s.mdt", pil->desc->name);
@@ -325,12 +333,11 @@ static int load_image(struct pil_device *pil)
 	if (ret) {
 		dev_err(&pil->dev, "%s: Failed to bring out of reset\n",
 				pil->desc->name);
-		proxy_timeout = 0; /* Remove proxy vote immediately on error */
 		goto err_boot;
 	}
 	dev_info(&pil->dev, "%s: Brought out of reset\n", pil->desc->name);
 err_boot:
-	pil_proxy_unvote(pil, proxy_timeout);
+	pil_proxy_unvote(pil, ret);
 release_fw:
 	release_firmware(fw);
 out: